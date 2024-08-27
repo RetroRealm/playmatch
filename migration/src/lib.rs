172a7a5f21cff4829f90fafaef4590551728afe5
Yukine <devyukine@gmx.de>
2024-08-27 17:46:27 +0200

    fix(db): correct unique indexes for signature_metadata_mapping table

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -9,6 +9,7 @@ mod m20240819_151911_add_signature_metadata_mapping_check_for_relation_id;
 mod m20240819_194749_add_parent_and_sibling_automatic_match_reason;
 mod m20240820_154703_add_signature_group_internal_clone_of_id;
 mod m20240823_145438_add_signature_metadata_mapping_unique_indexes;
+mod m20240827_153244_fix_signature_metadata_mapping_unique_indexes_to_take_provider_into_account;
 
 pub struct Migrator;
 
@@ -16,15 +17,16 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
 	fn migrations() -> Vec<Box<dyn MigrationTrait>> {
 		vec![
-            Box::new(m20240816_000001_initial_migration::Migration),
-            Box::new(m20240816_172957_insert_signature_group::Migration),
-            Box::new(m20240817_124421_create_updated_at_function_and_triggers::Migration),
-            Box::new(m20240819_001646_add_automatic_match_type_to_signature_metadata_matching::Migration),
-            Box::new(m20240819_151045_rename_signature_metadata_mapping_provider_name_to_provider::Migration),
-            Box::new(m20240819_151911_add_signature_metadata_mapping_check_for_relation_id::Migration),
-            Box::new(m20240819_194749_add_parent_and_sibling_automatic_match_reason::Migration),
-            Box::new(m20240820_154703_add_signature_group_internal_clone_of_id::Migration),
-            Box::new(m20240823_145438_add_signature_metadata_mapping_unique_indexes::Migration),
-        ]
+			Box::new(m20240816_000001_initial_migration::Migration),
+			Box::new(m20240816_172957_insert_signature_group::Migration),
+			Box::new(m20240817_124421_create_updated_at_function_and_triggers::Migration),
+			Box::new(m20240819_001646_add_automatic_match_type_to_signature_metadata_matching::Migration),
+			Box::new(m20240819_151045_rename_signature_metadata_mapping_provider_name_to_provider::Migration),
+			Box::new(m20240819_151911_add_signature_metadata_mapping_check_for_relation_id::Migration),
+			Box::new(m20240819_194749_add_parent_and_sibling_automatic_match_reason::Migration),
+			Box::new(m20240820_154703_add_signature_group_internal_clone_of_id::Migration),
+			Box::new(m20240823_145438_add_signature_metadata_mapping_unique_indexes::Migration),
+			Box::new(m20240827_153244_fix_signature_metadata_mapping_unique_indexes_to_take_provider_into_account::Migration),
+		]
 	}
 }
---
+++ migration/src/m20240827_153244_fix_signature_metadata_mapping_unique_indexes_to_take_provider_into_account.rs
@@ -0,0 +1,191 @@
+use sea_orm_migration::{prelude::*, sea_orm::Schema};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[derive(DeriveIden)]
+enum SignatureMetadataMapping {
+	Table,
+	GameId,
+	CompanyId,
+	PlatformId,
+	Provider,
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+	async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+		manager
+			.drop_index(
+				Index::drop()
+					.name("idx_signature_metadata_mapping_game_id_unique")
+					.to_owned(),
+			)
+			.await?;
+
+		manager
+			.create_index(
+				Index::create()
+					.name("idx_signature_metadata_mapping_game_id")
+					.table(SignatureMetadataMapping::Table)
+					.col(SignatureMetadataMapping::GameId)
+					.to_owned(),
+			)
+			.await?;
+
+		manager
+			.drop_index(
+				Index::drop()
+					.name("idx_signature_metadata_mapping_company_id_unique")
+					.to_owned(),
+			)
+			.await?;
+
+		manager
+			.create_index(
+				Index::create()
+					.name("idx_signature_metadata_mapping_company_id")
+					.table(SignatureMetadataMapping::Table)
+					.col(SignatureMetadataMapping::CompanyId)
+					.to_owned(),
+			)
+			.await?;
+
+		manager
+			.drop_index(
+				Index::drop()
+					.name("idx_signature_metadata_mapping_platform_id_unique")
+					.to_owned(),
+			)
+			.await?;
+
+		manager
+			.create_index(
+				Index::create()
+					.name("idx_signature_metadata_mapping_platform_id")
+					.table(SignatureMetadataMapping::Table)
+					.col(SignatureMetadataMapping::PlatformId)
+					.to_owned(),
+			)
+			.await?;
+
+		manager
+			.create_index(
+				Index::create()
+					.name("signature_metadata_mapping_company_id_provider_uindex")
+					.table(SignatureMetadataMapping::Table)
+					.col(SignatureMetadataMapping::CompanyId)
+					.col(SignatureMetadataMapping::Provider)
+					.unique()
+					.to_owned(),
+			)
+			.await?;
+
+		manager
+			.create_index(
+				Index::create()
+					.name("signature_metadata_mapping_game_id_provider_uindex")
+					.table(SignatureMetadataMapping::Table)
+					.col(SignatureMetadataMapping::GameId)
+					.col(SignatureMetadataMapping::Provider)
+					.unique()
+					.to_owned(),
+			)
+			.await?;
+
+		manager
+			.create_index(
+				Index::create()
+					.name("signature_metadata_mapping_provider_platform_id_uindex")
+					.table(SignatureMetadataMapping::Table)
+					.col(SignatureMetadataMapping::Provider)
+					.col(SignatureMetadataMapping::PlatformId)
+					.unique()
+					.to_owned(),
+			)
+			.await
+	}
+
+	async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+		manager
+			.drop_index(
+				Index::drop()
+					.name("signature_metadata_mapping_company_id_provider_uindex")
+					.to_owned(),
+			)
+			.await?;
+
+		manager
+			.drop_index(
+				Index::drop()
+					.name("signature_metadata_mapping_game_id_provider_uindex")
+					.to_owned(),
+			)
+			.await?;
+
+		manager
+			.drop_index(
+				Index::drop()
+					.name("signature_metadata_mapping_provider_platform_id_uindex")
+					.to_owned(),
+			)
+			.await?;
+
+		manager
+			.drop_index(
+				Index::drop()
+					.name("idx_signature_metadata_mapping_game_id")
+					.to_owned(),
+			)
+			.await?;
+
+		manager
+			.create_index(
+				Index::create()
+					.name("idx_signature_metadata_mapping_game_id_unique")
+					.table(SignatureMetadataMapping::Table)
+					.col(SignatureMetadataMapping::GameId)
+					.unique()
+					.to_owned(),
+			)
+			.await?;
+
+		manager
+			.drop_index(
+				Index::drop()
+					.name("idx_signature_metadata_mapping_company_id")
+					.to_owned(),
+			)
+			.await?;
+
+		manager
+			.create_index(
+				Index::create()
+					.name("idx_signature_metadata_mapping_company_id_unique")
+					.table(SignatureMetadataMapping::Table)
+					.col(SignatureMetadataMapping::CompanyId)
+					.unique()
+					.to_owned(),
+			)
+			.await?;
+
+		manager
+			.drop_index(
+				Index::drop()
+					.name("idx_signature_metadata_mapping_platform_id")
+					.to_owned(),
+			)
+			.await?;
+
+		manager
+			.create_index(
+				Index::create()
+					.name("idx_signature_metadata_mapping_platform_id_unique")
+					.table(SignatureMetadataMapping::Table)
+					.col(SignatureMetadataMapping::PlatformId)
+					.unique()
+					.to_owned(),
+			)
+			.await
+	}
+}
