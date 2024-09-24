2508738e4a765eeb23bdacfe46673e34420ac184
Yukine <devyukine@gmx.de>
2024-09-24 12:29:08 +0200

    clean(constants): use unwrap_or instead unwrap_or_else

--- service/src/constants/http.rs
+++ service/src/constants/http.rs
@@ -2,7 +2,7 @@ use lazy_static::lazy_static;
 
 lazy_static! {
 	pub static ref X_VERSION_HEADER_API: String =
-		std::env::var("X_VERSION_HEADER_API").unwrap_or_else(|_| "unknown".to_string());
+		std::env::var("X_VERSION_HEADER_API").unwrap_or("unknown".to_string());
 	pub static ref REQWEST_DEFAULT_USER_AGENT: String =
-		std::env::var("REQWEST_DEFAULT_USER_AGENT").unwrap_or_else(|_| "unknown".to_string());
+		std::env::var("REQWEST_DEFAULT_USER_AGENT").unwrap_or("unknown".to_string());
 }
