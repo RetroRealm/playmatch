335bdb0033f839cbea3e311729995f68be9fdeda
Yukine <devyukine@gmx.de>
2025-05-30 03:29:36 +0200

    fix(log): only log imported dat file when no error occurred

--- service/src/dat/mod.rs
+++ service/src/dat/mod.rs
@@ -133,8 +133,9 @@ pub async fn download_and_parse_dats(client: &Client, conn: &DbConn) -> anyhow::
 			parse_and_import_dat_file(&file, signature_group_entity.id, &hash, conn).await
 		{
 			error!("Failed to parse and import dat file: {:?}, {}", file, e);
+		} else {
+			info!("Imported DAT file: {}", file.display());
 		}
-		info!("Imported DAT file: {}", file.display());
 	}
 	info!("Finished importing all DAT files");
 
