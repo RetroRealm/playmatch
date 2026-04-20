3a8ee1c91178eb04f6eff251755dbce3a3911385
DevYukine <devyukine@gmx.de>
2026-04-20 07:38:23 +0200

    fix(db): default sqlx query logging back to debug level

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -100,7 +100,7 @@ async fn start() -> anyhow::Result<()> {
 	let sqlx_log_level = env::var("DATABASE_SQL_LOG_LEVEL")
 		.ok()
 		.and_then(|v| v.parse::<LevelFilter>().ok())
-		.unwrap_or(LevelFilter::Info);
+		.unwrap_or(LevelFilter::Debug);
 	opt.sqlx_logging_level(sqlx_log_level);
 	opt.sqlx_slow_statements_logging_settings(LevelFilter::Warn, Duration::from_secs(15));
 
